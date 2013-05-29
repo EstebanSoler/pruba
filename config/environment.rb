f691418ef0f921818e7c91a0bcc00dea51e0c30c
Esteban Soler <soleresteban@hotmail.com>
2013-05-29 17:11:48 -0300

    probando mail

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Depot::Application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+ :address => "smtp.gmail.com",
+ :port => 587,
+ :domain => "smtp.gmail.com" ,
+ :authentication => :login,
+ :user_name => "alfredoestebansoler" ,
+ :password => "cx0307245",
+ :enable_starttls_auto => false
+}
